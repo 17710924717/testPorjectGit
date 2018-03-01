bbd89b15f60e79fdabf786e249d00fe15dc000ed
zengqiang <18024588846.@163.com>
2018-03-01 15:35:36 +0800

    在studio端修改了MainActivity中添加了注释

--- app/src/main/java/soexample/umeng/com/testgituploadproject/MainActivity.java
+++ app/src/main/java/soexample/umeng/com/testgituploadproject/MainActivity.java
@@ -2,12 +2,14 @@ package soexample.umeng.com.testgituploadproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //tv值是在客户端上添加了
     }
 }
